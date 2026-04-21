affd17a43fce52611f91e8d7052ec75e065c2ae2
22 <a22@22deiMac.local>
2026-04-21 14:49:08 +0800

    first commit

---
+++ todoWIBISONO/todoWIBISONOApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoWIBISONOApp.swift
+//  todoWIBISONO
+//
+//  Created by 22 on 2026/4/21.
+//
+
+import SwiftUI
+
+@main
+struct todoWIBISONOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
